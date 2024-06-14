49bc039da3a69bdbc063e5d18d09c25cca5174b9
Jens Wittmann <post@jens-wittmann.de>
2024-06-14 17:02:07 +0200

    gitify extract

--- _gitify/snippets/getFileHash.php
+++ _gitify/snippets/getFileHash.php
@@ -2,8 +2,14 @@ id: 21
 source: 2
 name: getFileHash
 category: 9
-snippet: ''
+snippet: "if (file_exists(MODX_BASE_PATH.$f)) {\n    return substr(hash_file('md5', $f), 0, 3);\n}"
 properties: 'a:0:{}'
 static: 1
-static_file: /snippets/layout/getfilehash.snippet.php
-content: ''
+static_file: snippets/layout/getfilehash.snippet.php
+
+-----
+
+
+if (file_exists(MODX_BASE_PATH.$f)) {
+    return substr(hash_file('md5', $f), 0, 3);
+}
--- _gitify/snippets/svg.php
+++ _gitify/snippets/svg.php
@@ -2,10 +2,13 @@ id: 19
 source: 2
 name: svg
 category: 9
+snippet: "# vars\n$name = $modx->getOption( 'n', $scriptProperties );\n$scaleStroke = $modx->getOption( 'scalestroke', $scriptProperties, false );\n$path = $modx->getOption( 'filePath', $scriptProperties, $modx->getOption( 'tplPath' ). 'frontend/styleguide/icon/' );\n$extension = $modx->getOption( 'extension', $scriptProperties, '.svg' );\n$fullPath = $path . $name . $extension;\n\n# get file and modify\nif ( file_exists( $fullPath ) ) {\n    \n    $svg = file_get_contents( $fullPath );\n    $svg = str_replace( '<svg', '<svg focusable=\"false\" aria-hidden=\"true\"', $svg );\n    \n    if ( $scaleStroke ) {\n        $svg = str_replace( 'path', 'path vector-effect=\"non-scaling-stroke\"', $svg );\n    }\n    \n    return $svg;\n    \n}"
 properties: 'a:0:{}'
-static_file: /snippets/layout/svg.snippet.php
+static: 1
+static_file: snippets/layout/svg.snippet.php
 
 -----
+
 
 # vars
 $name = $modx->getOption( 'n', $scriptProperties );
@@ -16,14 +19,14 @@ $fullPath = $path . $name . $extension;
 
 # get file and modify
 if ( file_exists( $fullPath ) ) {
-	
-	$svg = file_get_contents( $fullPath );
-	$svg = str_replace( '<svg', '<svg focusable="false" aria-hidden="true"', $svg );
-	
-	if ( $scaleStroke ) {
-		$svg = str_replace( 'path', 'path vector-effect="non-scaling-stroke"', $svg );
-	}
-	
-	return $svg;
-	
+    
+    $svg = file_get_contents( $fullPath );
+    $svg = str_replace( '<svg', '<svg focusable="false" aria-hidden="true"', $svg );
+    
+    if ( $scaleStroke ) {
+        $svg = str_replace( 'path', 'path vector-effect="non-scaling-stroke"', $svg );
+    }
+    
+    return $svg;
+    
 }
