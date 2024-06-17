cc2b1f7905e7228fee7137136e989baa83a2be9f
Jens Wittmann <post@jens-wittmann.de>
2024-06-17 21:47:56 +0200

    managerModify: add parent id class to body

--- _gitify/plugins/managerModify.php
+++ _gitify/plugins/managerModify.php
@@ -2,7 +2,7 @@ id: 12
 source: 2
 name: managerModify
 category: 9
-plugincode: "if (!$modx->user->isMember(\"Administrator\")) {\n    $modx->regClientCSS(\n        MODX_BASE_URL .\n            $modx->getOption(\"tplPath\") .\n            \"frontend/styleguide/css/modx.css\"\n    );\n}"
+plugincode: "# add parent id class to body\n$resource = $modx->controller->resource;\nif ($resource) {\n    $templateId = $resource->get(\"template\");   \n    $script = '\n        <script>\n            window.addEventListener(\"load\", function(event) {\n                document.querySelector(\"body\").classList.add(\"template-id-' . $templateId . '\");\n            });\n        </script>\n    ';\n    $modx->regClientStartupHTMLBlock($script);\n}\n\n# add custom css\nif (!$modx->user->isMember(\"Administrator\")) {\n    $modx->regClientCSS(\n        MODX_BASE_URL .\n            $modx->getOption(\"tplPath\") .\n            \"frontend/styleguide/css/modx.css\"\n    );\n}"
 properties: 'a:0:{}'
 static: 1
 static_file: /plugins/layout/managermodify.plugin.php
@@ -10,6 +10,21 @@ static_file: /plugins/layout/managermodify.plugin.php
 -----
 
 
+# add parent id class to body
+$resource = $modx->controller->resource;
+if ($resource) {
+    $templateId = $resource->get("template");   
+    $script = '
+        <script>
+            window.addEventListener("load", function(event) {
+                document.querySelector("body").classList.add("template-id-' . $templateId . '");
+            });
+        </script>
+    ';
+    $modx->regClientStartupHTMLBlock($script);
+}
+
+# add custom css
 if (!$modx->user->isMember("Administrator")) {
     $modx->regClientCSS(
         MODX_BASE_URL .
--- assets/tpl/default/plugins/layout/managermodify.plugin.php
+++ assets/tpl/default/plugins/layout/managermodify.plugin.php
@@ -1,5 +1,20 @@
 <?php
 
+# add parent id class to body
+$resource = $modx->controller->resource;
+if ($resource) {
+    $templateId = $resource->get("template");   
+    $script = '
+        <script>
+            window.addEventListener("load", function(event) {
+                document.querySelector("body").classList.add("template-id-' . $templateId . '");
+            });
+        </script>
+    ';
+    $modx->regClientStartupHTMLBlock($script);
+}
+
+# add custom css
 if (!$modx->user->isMember("Administrator")) {
     $modx->regClientCSS(
         MODX_BASE_URL .
