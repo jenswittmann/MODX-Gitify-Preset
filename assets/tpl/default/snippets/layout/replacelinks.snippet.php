956202def0f2f72b393e0ef43173d45c91631373
Jens Wittmann – Gestaltung & Entwicklung <jenswittmann@users.noreply.github.com>
2024-07-22 21:21:09 +0200

    update replaceLinks snippet

--- assets/tpl/default/snippets/layout/replacelinks.snippet.php
+++ assets/tpl/default/snippets/layout/replacelinks.snippet.php
@@ -17,19 +17,6 @@ foreach ($dom->getElementsByTagName("a") as $a) {
     ) {
         $a->setAttribute("target", "_blank");
         $a->setAttribute("rel", "noreferer");
-        $nodeValWords = explode(" ", $a->nodeValue);
-        $nodeValLastWord = array_pop($nodeValWords);
-        $a->nodeValue = implode(" ", $nodeValWords) . " ";
-        $lastWordWrapper = $dom->createElement("span");
-        $lastWordWrapper->setAttribute("class", "dib nowrap");
-        $lastWordWrapper->nodeValue = $nodeValLastWord;
-        $iconWrapper = $dom->createElement("span");
-        $iconWrapper->setAttribute("class", "dib ml2 mr2");
-        $icon = $dom->createElement("span", "[[svg?n=`external`]]");
-        $icon->setAttribute("class", "icon icon-font");
-        $iconWrapper->appendChild($icon);
-        $lastWordWrapper->appendChild($iconWrapper);
-        $a->appendChild($lastWordWrapper);
     }
 }
 
