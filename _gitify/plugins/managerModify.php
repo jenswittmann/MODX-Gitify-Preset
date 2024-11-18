bb1356075d088931991c4cb1400e3483143aa9cd
Jens Wittmann <post@jens-wittmann.de>
2024-11-18 15:46:48 +0100

    managerModify: update plugin and add manager login custom css

--- _gitify/plugins/managerModify.php
+++ _gitify/plugins/managerModify.php
@@ -2,7 +2,7 @@ id: 12
 source: 2
 name: managerModify
 category: 9
-plugincode: "# add parent id class to body\n$resource = $modx->controller->resource;\nif ($resource) {\n    $templateId = $resource->get(\"template\");   \n    $script = '\n        <script>\n            window.addEventListener(\"load\", function(event) {\n                document.querySelector(\"body\").classList.add(\"template-id-' . $templateId . '\");\n            });\n        </script>\n    ';\n    $modx->regClientStartupHTMLBlock($script);\n}\n\n# add custom css\nif (!$modx->user->isMember(\"Administrator\")) {\n    $modx->regClientCSS(\n        MODX_BASE_URL .\n            $modx->getOption(\"tplPath\") .\n            \"frontend/styleguide/css/modx.css\"\n    );\n}"
+plugincode: "$cssPath = MODX_BASE_URL . $modx->getOption(\"tplPath\") . \"frontend/styleguide/css/modx.css\";\n\nswitch($modx->event->name) {\n    case 'OnManagerPageBeforeRender':\n        $modx->regClientCSS($cssPath);\n        break;\n\n    case 'OnManagerLoginFormPrerender':\n        $modx->event->output('<link rel=\"stylesheet\" href=\"' . $cssPath . '\">');\n        break;\n}"
 properties: 'a:0:{}'
 static: 1
 static_file: /plugins/layout/managermodify.plugin.php
@@ -10,25 +10,14 @@ static_file: /plugins/layout/managermodify.plugin.php
 -----
 
 
-# add parent id class to body
-// $resource = $modx->controller->resource;
-// if ($resource) {
-//     $templateId = $resource->get("template");
-//     $script = '
-//         <script>
-//             window.addEventListener("load", function(event) {
-//                 document.querySelector("body").classList.add("template-id-' . $templateId . '");
-//             });
-//         </script>
-//     ';
-//     $modx->regClientStartupHTMLBlock($script);
-// }
+$cssPath = MODX_BASE_URL . $modx->getOption("tplPath") . "frontend/styleguide/css/modx.css";
 
-# add custom css
-if (!$modx->user->isMember("Administrator")) {
-    $modx->regClientCSS(
-        MODX_BASE_URL .
-            $modx->getOption("tplPath") .
-            "frontend/styleguide/css/modx.css"
-    );
+switch($modx->event->name) {
+    case 'OnManagerPageBeforeRender':
+        $modx->regClientCSS($cssPath);
+        break;
+
+    case 'OnManagerLoginFormPrerender':
+        $modx->event->output('<link rel="stylesheet" href="' . $cssPath . '">');
+        break;
 }
--- assets/tpl/default/plugins/layout/managermodify.plugin.php
+++ assets/tpl/default/plugins/layout/managermodify.plugin.php
@@ -1,23 +1,12 @@
 <?php
-# add parent id class to body
-// $resource = $modx->controller->resource;
-// if ($resource) {
-//     $templateId = $resource->get("template");
-//     $script = '
-//         <script>
-//             window.addEventListener("load", function(event) {
-//                 document.querySelector("body").classList.add("template-id-' . $templateId . '");
-//             });
-//         </script>
-//     ';
-//     $modx->regClientStartupHTMLBlock($script);
-// }
+$cssPath = MODX_BASE_URL . $modx->getOption("tplPath") . "frontend/styleguide/css/modx.css";
 
-# add custom css
-if (!$modx->user->isMember("Administrator")) {
-    $modx->regClientCSS(
-        MODX_BASE_URL .
-            $modx->getOption("tplPath") .
-            "frontend/styleguide/css/modx.css"
-    );
+switch($modx->event->name) {
+    case 'OnManagerPageBeforeRender':
+        $modx->regClientCSS($cssPath);
+        break;
+
+    case 'OnManagerLoginFormPrerender':
+        $modx->event->output('<link rel="stylesheet" href="' . $cssPath . '">');
+        break;
 }
