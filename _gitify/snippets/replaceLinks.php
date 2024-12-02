cd0b1f661948f0305f59ef710b16ba44cd20b3d1
Jens Wittmann <post@jens-wittmann.de>
2024-12-02 21:06:07 +0100

    gitify extract

--- _gitify/snippets/replaceLinks.php
+++ _gitify/snippets/replaceLinks.php
@@ -2,7 +2,6 @@ id: 20
 source: 2
 name: replaceLinks
 category: 9
-snippet: "if (empty($input)) {\n    return;\n}\n    \n$dom = new DOMDocument();\n$dom->loadHTML(mb_convert_encoding($input, \"HTML-ENTITIES\", \"UTF-8\"));\nforeach ($dom->getElementsByTagName(\"a\") as $a) {\n    $a->setAttribute(\n        \"href\",\n        str_replace($modx->getOption(\"site_url\"), \"\", $a->getAttribute(\"href\"))\n    );\n    $a->setAttribute(\"class\", \"link\");\n    $a->removeAttribute(\"style\");\n    if (\n        substr($a->getAttribute(\"href\"), 0, 4) == \"http\" ||\n        substr($a->getAttribute(\"href\"), -3) == \"pdf\"\n    ) {\n        $a->setAttribute(\"target\", \"_blank\");\n        $a->setAttribute(\"rel\", \"noreferer\");\n        $a->setAttribute(\"title\", \"externer Link\");\n    }\n}\n\nreturn preg_replace(\n    \"~<(?:!DOCTYPE|/?(?:html|body))[^>]*>\\s*~i\",\n    \"\",\n    $dom->saveHTML()\n);"
 properties: 'a:0:{}'
 static: 1
 static_file: snippets/layout/replacelinks.snippet.php
